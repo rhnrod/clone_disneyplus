c7d21b6288d38abd60d6aafa49af13db81b4c5a5
Rhuan Emanuel <rhnrod@gmail.com>
2023-09-09 09:14:00 -0300

    add faq section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(btn) {
@@ -11,7 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
             btn.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++ ) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(el) {
+    const classe = 'faq__questions__item--is-open';
+    const parent = el.target.parentNode;
+
+    parent.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
